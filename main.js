e18eda012908a4fc749cfd630615730df23d5fa3
thenumbernine <christopher.e.moore@gmail.com>
2024-07-20 17:18:07 +0600

    one more folder

--- main.js
+++ main.js
@@ -339,6 +339,7 @@ await Promise.all([
 	addLuaDir('glapp/tests', ['compute.lua', 'compute-spirv.lua', 'cubemap.lua', 'events.lua', 'info.lua', 'minimal.lua', 'pointtest.lua', 'test_es_directcalls.lua', 'test_tex.lua', 'test_es.lua', 'test.lua', 'test_vertexattrib.lua', 'src.png']),
 	addLuaDir('imgui', ['imgui.lua']),
 	addLuaDir('imguiapp', ['imguiapp.lua', 'withorbit.lua']),
+	addLuaDir('imguiapp/tests', ['console.lua', 'demo.lua', 'font.lua']),
 	addLuaDir('line-integral-convolution', ['run.lua']),
 	addLuaDir('rule110', ['rule110.lua']),
 	addLuaDir('n-points', ['run.lua', 'run_orbit.lua']),
